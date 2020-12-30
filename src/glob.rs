e10b68a110aa726de9329e98d699423ba82e7598
Sean Olson <olson.sean.k@gmail.com>
2020-12-29 23:34:54 -0800

    Improve regular expression encoding of globs.

--- src/glob.rs
+++ src/glob.rs
@@ -1,11 +1,13 @@
 use bstr::ByteVec;
-use itertools::Itertools as _;
+use itertools::{Itertools as _, Position};
 use nom::error::ErrorKind;
 use regex::bytes::Regex;
 use std::borrow::Cow;
 use std::path::Path;
 use std::str::FromStr;
 use thiserror::Error;
+
+use crate::PositionExt as _;
 
 #[derive(Debug, Error)]
 pub enum GlobError {
@@ -117,26 +119,30 @@ impl<'a> Glob<'a> {
             })
             .collect();
         let mut pattern = String::new();
-        pattern.push_str("(?-u)^");
-        for token in tokens.iter() {
-            match token {
-                Token::Literal(ref literal) => {
+        let mut push = |text: &str| pattern.push_str(text);
+        push("(?-u)^");
+        for token in tokens.iter().with_position() {
+            match token.lift() {
+                (_, Token::Literal(ref literal)) => {
                     let bytes = literal.as_bytes();
                     for &byte in bytes {
                         if byte <= ASCII_TERMINATOR {
-                            pattern.push_str(&regex::escape(&(byte as char).to_string()));
+                            push(&regex::escape(&(byte as char).to_string()));
                         }
                         else {
-                            pattern.push_str(&format!("\\x{:02x}", byte));
+                            push(&format!("\\x{:02x}", byte));
                         }
                     }
                 }
-                Token::Wildcard(Wildcard::One) => pattern.push_str("([^/])"),
-                Token::Wildcard(Wildcard::Many) => pattern.push_str("([^/]*)"),
-                Token::Wildcard(Wildcard::Tree) => pattern.push_str("(/|/.*/)"),
+                (_, Token::Wildcard(Wildcard::One)) => push("([^/])"),
+                (_, Token::Wildcard(Wildcard::Many)) => push("([^/]*)"),
+                (Position::First(()), Token::Wildcard(Wildcard::Tree)) => push("(/?|.*/)"),
+                (Position::Middle(()), Token::Wildcard(Wildcard::Tree)) => push("(/|/.*/)"),
+                (Position::Last(()), Token::Wildcard(Wildcard::Tree)) => push("(/?|/.*)"),
+                (Position::Only(()), Token::Wildcard(Wildcard::Tree)) => push("(.*)"),
             }
         }
-        pattern.push_str("$");
+        push("$");
         let regex = Regex::new(&pattern).map_err(|_| GlobError::Parse)?;
         Ok(Glob { tokens, regex })
     }
@@ -144,13 +150,26 @@ impl<'a> Glob<'a> {
     pub fn parse(text: &'a str) -> Result<Self, GlobError> {
         use nom::bytes::complete as bytes;
         use nom::error::{FromExternalError, ParseError};
-        use nom::{branch, combinator, multi, sequence, IResult};
+        use nom::{branch, combinator, multi, sequence, IResult, Parser};
 
+        fn no_adjacent_tree<'i, O, E, F>(parser: F) -> impl FnMut(&'i str) -> IResult<&'i str, O, E>
+        where
+            E: ParseError<&'i str>,
+            F: Parser<&'i str, O, E>,
+        {
+            sequence::delimited(
+                combinator::peek(combinator::not(bytes::tag("**"))),
+                parser,
+                combinator::peek(combinator::not(bytes::tag("**"))),
+            )
+        }
+
+        // TODO: Support escaping wildcards.
         fn literal<'i, E>(input: &'i str) -> IResult<&'i str, Token, E>
         where
             E: ParseError<&'i str>,
         {
-            combinator::map(bytes::is_not("?*"), From::from)(input)
+            combinator::map(no_adjacent_tree(bytes::is_not("/?*")), From::from)(input)
         }
 
         fn wildcard<'i, E>(input: &'i str) -> IResult<&'i str, Token, E>
@@ -158,17 +177,29 @@ impl<'a> Glob<'a> {
             E: ParseError<&'i str>,
         {
             branch::alt((
-                combinator::map(bytes::tag("?"), |_| Token::from(Wildcard::One)),
-                // TODO: Only allow path separators as delimiters of tree wildcards.
-                sequence::terminated(
-                    combinator::map(bytes::tag("**"), |_| Token::from(Wildcard::Tree)),
-                    branch::alt((bytes::is_not("*"), combinator::eof)),
+                combinator::map(no_adjacent_tree(bytes::tag("?")), |_| {
+                    Token::from(Wildcard::One)
+                }),
+                combinator::map(
+                    sequence::delimited(
+                        branch::alt((bytes::tag("/"), bytes::tag(""))),
+                        bytes::tag("**"),
+                        branch::alt((bytes::tag("/"), combinator::eof)),
+                    ),
+                    |_| Token::from(Wildcard::Tree),
                 ),
                 sequence::terminated(
                     combinator::map(bytes::tag("*"), |_| Token::from(Wildcard::Many)),
-                    branch::alt((bytes::is_not("*"), combinator::eof)),
+                    branch::alt((combinator::peek(bytes::is_not("*")), combinator::eof)),
                 ),
             ))(input)
+        }
+
+        fn separator<'i, E>(input: &'i str) -> IResult<&'i str, Token, E>
+        where
+            E: ParseError<&'i str>,
+        {
+            combinator::map(bytes::tag("/"), From::from)(input)
         }
 
         fn glob<'i, E>(input: &'i str) -> IResult<&'i str, Glob, E>
@@ -176,8 +207,8 @@ impl<'a> Glob<'a> {
             E: FromExternalError<&'i str, GlobError> + ParseError<&'i str>,
         {
             combinator::all_consuming(combinator::map_res(
-                multi::many1(branch::alt((literal, wildcard))),
-                move |tokens| Glob::from_tokens(tokens),
+                multi::many1(branch::alt((literal, wildcard, separator))),
+                |tokens| Glob::from_tokens(tokens),
             ))(input)
         }
 
@@ -208,37 +239,68 @@ mod tests {
     use crate::glob::Glob;
 
     #[test]
-    fn parse_any_tokens() {
+    fn parse_glob_with_any_tokens() {
         Glob::parse("*").unwrap();
         Glob::parse("a/*").unwrap();
         Glob::parse("*a").unwrap();
         Glob::parse("a*").unwrap();
         Glob::parse("a*b").unwrap();
+        Glob::parse("/*").unwrap();
     }
 
     #[test]
-    fn parse_one_tokens() {
+    fn parse_glob_with_one_tokens() {
         Glob::parse("?").unwrap();
         Glob::parse("a/?").unwrap();
         Glob::parse("?a").unwrap();
         Glob::parse("a?").unwrap();
         Glob::parse("a?b").unwrap();
         Glob::parse("??a??b??").unwrap();
+        Glob::parse("/?").unwrap();
     }
 
     #[test]
-    fn parse_tree_tokens() {
+    fn parse_glob_with_any_and_one_tokens() {
+        Glob::parse("?*").unwrap();
+        Glob::parse("*?").unwrap();
+        Glob::parse("*/?").unwrap();
+        Glob::parse("?*?").unwrap();
+        Glob::parse("/?*").unwrap();
+    }
+
+    #[test]
+    fn parse_glob_with_tree_tokens() {
         Glob::parse("**").unwrap();
         Glob::parse("**/").unwrap();
         Glob::parse("/**").unwrap();
         Glob::parse("**/a").unwrap();
         Glob::parse("a/**").unwrap();
+        Glob::parse("**/a/**/b/**").unwrap();
+        Glob::parse("**/**/a").unwrap();
     }
 
     #[test]
-    fn reject_adjacent_any_or_tree_tokens() {
+    fn reject_glob_with_adjacent_any_or_tree_tokens() {
         assert!(Glob::parse("***").is_err());
         assert!(Glob::parse("****").is_err());
         assert!(Glob::parse("**/*/***").is_err());
+    }
+
+    #[test]
+    fn reject_glob_with_adjacent_literal_tokens() {
+        assert!(Glob::parse("**a").is_err());
+        assert!(Glob::parse("a**").is_err());
+        assert!(Glob::parse("a**b").is_err());
+        assert!(Glob::parse("a*b**").is_err());
+        assert!(Glob::parse("**/**a/**").is_err());
+    }
+
+    #[test]
+    fn reject_glob_with_adjacent_one_tokens() {
+        assert!(Glob::parse("**?").is_err());
+        assert!(Glob::parse("?**").is_err());
+        assert!(Glob::parse("?**?").is_err());
+        assert!(Glob::parse("?*?**").is_err());
+        assert!(Glob::parse("**/**?/**").is_err());
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,20 @@ pub mod manifest;
 pub mod pattern;
 pub mod policy;
 pub mod transform;
+
+use itertools::Position;
+
+trait PositionExt<T> {
+    fn lift(&self) -> (Position<()>, &T);
+}
+
+impl<T> PositionExt<T> for Position<T> {
+    fn lift(&self) -> (Position<()>, &T) {
+        match *self {
+            Position::First(ref inner) => (Position::First(()), inner),
+            Position::Middle(ref inner) => (Position::Middle(()), inner),
+            Position::Last(ref inner) => (Position::Last(()), inner),
+            Position::Only(ref inner) => (Position::Only(()), inner),
+        }
+    }
+}
