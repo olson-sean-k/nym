6fda3708fb620406a100a117674e3bedfc0ef820
Sean Olson <olson.sean.k@gmail.com>
2021-02-10 09:36:18 -0800

    Include nom error information in `PatternError::Parse`.

--- src/pattern/mod.rs
+++ src/pattern/mod.rs
@@ -14,16 +14,16 @@ pub use crate::pattern::to::ToPattern;
 pub enum PatternError {
     #[error("capture not found in from-pattern")]
     CaptureNotFound,
-    #[error("failed to parse pattern")]
-    Parse,
+    #[error("failed to parse pattern: {0}")]
+    Parse(nom::Err<(String, ErrorKind)>),
     #[error("failed to encode capture in to-pattern: {0}")]
     Encoding(Utf8Error),
     #[error("failed to read property in to-pattern: {0}")]
     Property(io::Error),
 }
 
-impl<I> From<nom::Err<(I, ErrorKind)>> for PatternError {
-    fn from(_: nom::Err<(I, ErrorKind)>) -> Self {
-        PatternError::Parse
+impl<'i> From<nom::Err<(&'i str, ErrorKind)>> for PatternError {
+    fn from(error: nom::Err<(&'i str, ErrorKind)>) -> Self {
+        PatternError::Parse(error.to_owned())
     }
 }
